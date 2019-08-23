98d2f5128f11a9850ab370ed95d04b7a19815dde
Ryosuke Uchitate <ruchitate@coiney.com>
2019-08-23 18:42:16 +0900

    フッター修正

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,67 +14,35 @@ const Footer = class extends React.Component {
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
             <div className="columns">
-              <div className="column is-4">
-                <section className="menu">
-                  <ul className="menu-list">
-                    <li>
-                      <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/about">
-                        About
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
-                      </Link>
-                    </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
-                      </a>
-                    </li>
-                  </ul>
+              <div className="column is-6">
+                <section>
+                  <h3>個人情報の取り扱い</h3>
+                  <p>お申込戴きました情報は個人を特定できる状態で、本団体以外の第三者に開示することはございません。 (法律により開示が要求された場合、または他のお客様、あるいはイベントに関係する団体・個人の権利などを守る場合に必要という判断が合理的にされる場合を除きます) </p>
                 </section>
               </div>
               <div className="column is-4">
                 <section>
-                  <ul className="menu-list">
-                    <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact">
-                        Contact
-                      </Link>
-                    </li>
-                  </ul>
+                  <h3>About Us</h3>
+                  <p>
+                    <a href="http://www.java-users.jp/?page_id=7" target="_blank">JJUGについて</a>
+                  </p>
+                  <p>
+                    <a href="http://www.java-users.jp/?page_id=18" target="_blank">入会案内</a>
+                  </p>
+                  <p>
+                    <a href="http://www.java-users.jp/?page_id=166" target="_blank">幹事会メンバー</a>
+                  </p>
                 </section>
               </div>
-              <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
+              <div className="column is-2 social">
+                <a title="facebook" href="https://www.facebook.com/japanjug/" target="_blank">
                   <img
                     src={facebook}
                     alt="Facebook"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="twitter" href="https://twitter.com">
+                <a title="twitter" href="https://twitter.com/jjug" target="_blank">
                   <img
                     className="fas fa-lg"
                     src={twitter}
@@ -82,21 +50,10 @@ const Footer = class extends React.Component {
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="instagram" href="https://instagram.com">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
               </div>
+            </div>
+            <div>
+              <p>© Copyright 2019 JJUG - All rights reserved. </p>
             </div>
           </div>
         </div>
